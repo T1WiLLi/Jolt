a71ceed8e6faf4c4487b7430fb1ccc8c6a3ed829
T1WiLLi <williambeaudun@gmail.com>
2025-03-10 09:50:23 -0400

    testing.

--- src/main/java/ca/jolt/Main.java
+++ src/main/java/ca/jolt/Main.java
@@ -3,6 +3,7 @@ package ca.jolt;
 import java.util.Map;
 
 import ca.jolt.core.JoltApplication;
+import ca.jolt.form.Form;
 import ca.jolt.http.HttpStatus;
 import ca.jolt.routing.context.JoltContext;
 
@@ -15,9 +16,49 @@ public class Main extends JoltApplication {
     public void setup() {
         get("/", Main::getUser);
         get("/login", ctx -> ctx.serve("login.html"));
+        post("/login", ctx -> {
+            Form form = ctx.buildForm();
+            form.field("username")
+                    .required();
+            form.field("password")
+                    .required();
+
+            if (form.verify()) {
+                if (form.getValue("username").equals("admin") && form.getValue("password").equals("password")) {
+                    return ctx.html("You are logged in!").ok();
+                } else {
+                    return ctx.html("Wrong password or username!");
+                }
+            } else {
+                return ctx.html("<h1>Error on form submission!</h1> <br> <p>" + form.getAllErrors() + "</p>")
+                        .status(HttpStatus.BAD_REQUEST);
+            }
+        });
     }
 
     public static JoltContext getUser(JoltContext ctx) {
         return ctx.status(HttpStatus.OK).json(Map.of("name", "John Doe", "age", 30));
     }
 }
+/*
+ * 
+ * {
+ * Form form = ctx.buildForm();
+ * form.field("username")
+ * .required();
+ * form.field("password")
+ * .required();
+ * 
+ * if (form.verify()) {
+ * if (form.getValue("username").equals("admin") &&
+ * form.getValue("password").equals("password")) {
+ * ctx.html("<h1>Logged in!</h1>");
+ * } else {
+ * ctx.html("<h1>Invalid username or password!</h1>");
+ * }
+ * } else {
+ * ctx.html("<h1>Invalid login!</h1>" + "<p>" + form.getAllErrors() + "</p>");
+ * }
+ * }
+ * 
+ */
