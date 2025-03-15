a7600184758fa95fb6a27e49cacd764d2d952794
William Beaudin <williambeaudun@gmail.com>
2025-03-15 11:44:14 -0400

    update.

---
+++ src/main/java/ca/jolt/Main.java
@@ -0,0 +1,80 @@
+
+package ca.jolt;
+
+import java.util.Map;
+import java.util.Optional;
+
+import ca.jolt.core.JoltApplication;
+import ca.jolt.database.Broker;
+import ca.jolt.database.Database;
+import ca.jolt.database.annotation.Id;
+import ca.jolt.database.annotation.Table;
+import ca.jolt.form.Form;
+import ca.jolt.http.HttpStatus;
+import ca.jolt.routing.context.JoltContext;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class Main extends JoltApplication {
+    public static void main(String[] args) {
+        launch(Main.class, "ca.jolt");
+    }
+
+    @Override
+    public void setup() {
+        Database.init();
+        get("/", Main::getUser);
+        group("/user", () -> {
+            get("/{name}", ctx -> ctx.json(new UserBroker().findByName(ctx.path("name").get()).get()));
+            get("", ctx -> ctx.json(new UserBroker().findAll()));
+            post("", ctx -> {
+                Form form = ctx.buildForm();
+                User user = form.buildEntity(User.class);
+                new UserBroker().save(user);
+                return ctx.created().json(user);
+            });
+            put("/{id:int}", ctx -> {
+                Form form = ctx.buildForm();
+
+                form.field("name")
+                        .required()
+                        .minLength(3);
+                if (!form.verify()) {
+                    return ctx.status(HttpStatus.BAD_REQUEST).json(form.getErrors());
+                }
+
+                User user = new UserBroker().findById(ctx.path("id").asInt()).get();
+                new UserBroker().save(form.updateEntity(user));
+                return ctx.ok().json(Map.of("Message", "User update"));
+            });
+        });
+    }
+
+    public static JoltContext getUser(JoltContext ctx) {
+        return ctx.json(Map.of("name", "John Doe", "age", 30));
+    }
+
+    public static class UserBroker extends Broker<Integer, User> {
+        public Optional<User> findByName(String name) {
+            return selectSingle("WHERE name = ?", name);
+        }
+    }
+
+    @NoArgsConstructor
+    @Table(table = "users")
+    @Getter
+    @Setter
+    public static class User {
+        @Id
+        private Integer id;
+
+        private String name;
+        private String email;
+
+        public User(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    }
+}
