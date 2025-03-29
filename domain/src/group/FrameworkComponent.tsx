8fdaf3e4be74761d0f1236fd8cc87354766064ef
T1WiLLi <williambeaudun@gmail.com>
2025-03-29 19:26:42 -0400

    feat(Logging): Logging documentation section.

--- domain/src/group/FrameworkComponent.tsx
+++ domain/src/group/FrameworkComponent.tsx
@@ -1,9 +1,11 @@
 import Filters from "../sections/fc/Filters"
+import Logging from "../sections/fc/Logging"
 
 function FrameworkComponent() {
     return (
         <div className="flex flex-col gap-5">
             <Filters />
+            <Logging />
         </div>
     )
 }
