bc5b668797f66b7e62614fb264a1249d700d971d
William Beaudin <williambeaudun@gmail.com>
2025-04-16 19:02:55 -0400

    feat: Updated Template configuration to escape to HTML.

--- src/main/java/io/github/t1willi/Main.java
+++ src/main/java/io/github/t1willi/Main.java
@@ -1,7 +1,6 @@
 package io.github.t1willi;
 
 import io.github.t1willi.core.JoltApplication;
-import io.github.t1willi.files.JoltFile;
 
 public class Main extends JoltApplication {
     public static void main(String[] args) {
--- src/main/java/io/github/t1willi/template/TemplateConfiguration.java
+++ src/main/java/io/github/t1willi/template/TemplateConfiguration.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import freemarker.core.HTMLOutputFormat;
 import freemarker.template.Configuration;
 import freemarker.template.TemplateExceptionHandler;
 import io.github.t1willi.exceptions.TemplatingException;
@@ -40,6 +41,8 @@ public abstract class TemplateConfiguration {
         this.configuration = new Configuration(Configuration.VERSION_2_3_32);
         this.configuration.setDefaultEncoding("UTF-8");
         this.configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        this.configuration.setIncompatibleImprovements(Configuration.VERSION_2_3_32);
+        this.configuration.setOutputFormat(HTMLOutputFormat.INSTANCE);
         this.configuration.setLogTemplateExceptions(true);
         this.configuration.setWrapUncheckedExceptions(true);
         this.configuration.setSharedVariable("nonce", new NonceDirective());
