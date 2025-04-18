fbe212f8ac63715f33d789485def98e3d1b35beb
T1WiLLi <williambeaudun@gmail.com>
2025-04-18 00:35:15 -0400

    feat: Removed the necessity to explicitly tell Jolt component the Main class for the application.

--- src/main/java/io/github/t1willi/Main.java
+++ src/main/java/io/github/t1willi/Main.java
@@ -4,7 +4,7 @@ import io.github.t1willi.core.JoltApplication;
 
 public class Main extends JoltApplication {
     public static void main(String[] args) {
-        launch(Main.class);
+        launch();
     }
 
     @Override
