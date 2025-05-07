90753204e6c790d7197de74222c0868a2ef7e42a
William Beaudin <williambeaudun@gmail.com>
2025-05-07 15:05:13 -0400

    chore(pom): Updated compiler process to retain parameter name instead of generic 'arg01, arg02' ect.

--- src/main/java/io/github/t1willi/HomeController.java
+++ src/main/java/io/github/t1willi/HomeController.java
@@ -2,6 +2,8 @@ package io.github.t1willi;
 
 import io.github.t1willi.annotations.Controller;
 import io.github.t1willi.annotations.Get;
+import io.github.t1willi.annotations.Path;
+import io.github.t1willi.annotations.Query;
 import io.github.t1willi.annotations.Version;
 import io.github.t1willi.core.BaseController;
 
@@ -18,5 +20,15 @@ public class HomeController extends BaseController {
     @Version(value = 3, prefix = "ver")
     public String homeV3() {
         return "This a way better version, we are touching V3";
+    }
+
+    @Get("/user")
+    public String user(@Query String name) {
+        return "Hello " + name;
+    }
+
+    @Get("/user/{name}")
+    public String userPath(@Path String name) {
+        return "Hello " + name;
     }
 }
