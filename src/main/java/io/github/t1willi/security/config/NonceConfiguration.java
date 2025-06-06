6ae5deeab255cfb18b2b7bf556aedfd2f8e407cc
T1WiLLi <williambeaudun@gmail.com>
2025-06-06 15:32:40 -0400

    feat: Add support for excluding URL patterns in NonceConfiguration

--- src/main/java/io/github/t1willi/security/config/NonceConfiguration.java
+++ src/main/java/io/github/t1willi/security/config/NonceConfiguration.java
@@ -1,14 +1,19 @@
 package io.github.t1willi.security.config;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class NonceConfiguration {
 
     private final SecurityConfiguration parent;
-
     private boolean enabled;
+    private final Set<String> excludedUrlPatterns;
 
     public NonceConfiguration(SecurityConfiguration parent) {
         this.parent = parent;
         this.enabled = false;
+        this.excludedUrlPatterns = new HashSet<>();
     }
 
     public SecurityConfiguration and() {
@@ -20,7 +25,16 @@ public class NonceConfiguration {
         return this;
     }
 
+    public NonceConfiguration excludeUrlPatterns(String... excludedUrls) {
+        Collections.addAll(this.excludedUrlPatterns, excludedUrls);
+        return this;
+    }
+
     public boolean isEnabled() {
         return enabled;
+    }
+
+    public Set<String> getExcludedUrlPatterns() {
+        return Collections.unmodifiableSet(excludedUrlPatterns);
     }
 }
