ce2b1c3b3f8d60f670207aec864e6a62ca87ee0e
William Beaudin <williambeaudun@gmail.com>
2025-07-14 23:15:09 -0400

    feat: hotfix for the encoding pipeline

---
+++ src/main/java/io/github/t1willi/Main.java
@@ -0,0 +1,15 @@
+package io.github.t1willi;
+
+import io.github.t1willi.core.JoltApplication;
+
+public class Main extends JoltApplication {
+    public static void main(String[] args) {
+        launch(Main.class);
+    }
+
+    public void init() {
+        get("/", ctx -> {
+            return ctx.html("Hello World!");
+        });
+    }
+}
--- src/main/java/io/github/t1willi/pipeline/EncodingStep.java
+++ src/main/java/io/github/t1willi/pipeline/EncodingStep.java
@@ -3,17 +3,13 @@ package io.github.t1willi.pipeline;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public class EncodingStep implements PipelineStep {
 
     @Override
     public boolean execute(ProcessingContext context) throws IOException, ServletException {
-        HttpServletRequest request = context.getRequest();
-        HttpServletResponse response = context.getResponse();
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        return true;
+        context.getRequest().setCharacterEncoding("UTF-8");
+        context.getResponse().setCharacterEncoding("UTF-8");
+        return false;
     }
 }
